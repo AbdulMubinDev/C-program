05c6341f024d05ec45ee09116c31fb1fac057b31
AbdulMubinDev <abdulmubin.dev@gmail.com>
2024-12-13 18:07:36 +0600

    some update

--- calculator.c
+++ calculator.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-void menu(){
-    
+void menu() {
     printf("Welcome to this beginner project made by @AbdulMubinDev\n\n");
     printf("=========================================\n");
     printf("|                   MENU                |\n");
@@ -14,94 +13,171 @@ void menu(){
     printf("========================================\n");
 }
 
-int addition(){
-    int num1, num2, result; 
-
-    printf("Let's do some Addition :) ");
-    printf("\nFirst enter your first number: ");
-    scanf("%d", &num1);
-
-    printf("Now enter the second number: ");
-    scanf("%d", &num2);
-
-    result = num1 + num2; 
-    printf("This is your result: %d", result);
-}
-
-int subtraction(){
-    int num1, num2, result; 
-
-    printf("Let's do some Substraction :) ");
-    printf("\nFirst enter your first number: ");
-    scanf("%d", &num1);
-
-    printf("Now enter the second number: ");
-    scanf("%d", &num2);
-
-    result = num1 - num2; 
-    printf("This is your result: %d", result);
-}
-
-int multiplication(){
-    int num1, num2, result; 
-
-    printf("Let's do some Multiplication :) ");
-    printf("\nFirst enter your first number: ");
-    scanf("%d", &num1);
-
-    printf("Now enter the second number: ");
-    scanf("%d", &num2);
-
-    result = num1 * num2; 
-    printf("This is your result: %d", result);
-}
-
-int division(){
-    int num1, num2, result; 
-
-    printf("Let's do some Division :) ");
-    printf("\nFirst enter your first number: ");
-    scanf("%d", &num1);
-
-    printf("Now enter the second number: ");
-    scanf("%d", &num2);
-
-    result = num1 / num2; 
-    printf("This is your result: %d", result);
-}
-
-
-int main(){
-
-    int option; 
-
-    menu();
-
-    printf("\nPlease select one option for basic mathmatics operations: ");
-    scanf("%d", &option);
-
-    switch (option)
-    {
-    case 1 :
-        addition();
-        break;
-    case 2 :
-        subtraction();
-        break;
-    case 3 : 
-        multiplication();
-        break;
-    case 4 :
-        division();
-        break;  
-    case 0 :
-        printf("Bye see you again :)");
-        return 0;
-        break;
-
-    default:
-        printf("Opss you maybe enter wrong option :} Try again");
-        break;
+double getNumber() {
+    double num;
+    while (1) {
+        if (scanf("%lf", &num) == 1) {
+            return num;
+        } else {
+            printf("\nInvalid input! Please enter a valid number: ");
+            while (getchar() != '\n'); // Clear input buffer
+        }
     }
-    return 0; 
+}
+
+int addition() {
+    double num1, num2, result;
+    char choice;
+    printf("Let's do some Addition :)\n");
+    printf("First enter your first number: ");
+    num1 = getNumber();
+    printf("Now enter the second number: ");
+    num2 = getNumber();
+    result = num1 + num2;
+    printf("This is your result: %.2lf\n", result);
+
+    while (1) {
+        printf("Press # to return to the main menu, 0 to close the program, or 'c' to redo addition: ");
+        scanf(" %c", &choice);
+        while (getchar() != '\n'); // Clear input buffer
+
+        if (choice == '#') {
+            return 1;
+        } else if (choice == '0') {
+            printf("Bye! See you again :)\n");
+            return 0;
+        } else if (choice == 'c') {
+            return addition();
+        } else {
+            printf("Invalid option! Please try again.\n");
+        }
+    }
+}
+
+int subtraction() {
+    double num1, num2, result;
+    char choice;
+    printf("Let's do some Subtraction :)\n");
+    printf("First enter your first number: ");
+    num1 = getNumber();
+    printf("Now enter the second number: ");
+    num2 = getNumber();
+    result = num1 - num2;
+    printf("This is your result: %.2lf\n", result);
+
+    while (1) {
+        printf("Press # to return to the main menu, 0 to close the program, or 'c' to redo subtraction: ");
+        scanf(" %c", &choice);
+        while (getchar() != '\n'); // Clear input buffer
+
+        if (choice == '#') {
+            return 1;
+        } else if (choice == '0') {
+            printf("Bye! See you again :)\n");
+            return 0;
+        } else if (choice == 'c') {
+            return subtraction();
+        } else {
+            printf("Invalid option! Please try again.\n");
+        }
+    }
+}
+
+int multiplication() {
+    double num1, num2, result;
+    char choice;
+    printf("Let's do some Multiplication :)\n");
+    printf("First enter your first number: ");
+    num1 = getNumber();
+    printf("Now enter the second number: ");
+    num2 = getNumber();
+    result = num1 * num2;
+    printf("This is your result: %.2lf\n", result);
+
+    while (1) {
+        printf("Press # to return to the main menu, 0 to close the program, or 'c' to redo multiplication: ");
+        scanf(" %c", &choice);
+        while (getchar() != '\n'); // Clear input buffer
+
+        if (choice == '#') {
+            return 1;
+        } else if (choice == '0') {
+            printf("Bye! See you again :)\n");
+            return 0;
+        } else if (choice == 'c') {
+            return multiplication();
+        } else {
+            printf("Invalid option! Please try again.\n");
+        }
+    }
+}
+
+int division() {
+    double num1, num2, result;
+    char choice;
+    printf("Let's do some Division :)\n");
+    printf("First enter your first number: ");
+    num1 = getNumber();
+    printf("Now enter the second number: ");
+    while (1) {
+        num2 = getNumber();
+        if (num2 != 0) break;
+        printf("Division by zero is not allowed! Please enter another number: ");
+    }
+    result = num1 / num2;
+    printf("This is your result: %.2lf\n", result);
+
+    while (1) {
+        printf("Press # to return to the main menu, 0 to close the program, or 'c' to redo division: ");
+        scanf(" %c", &choice);
+        while (getchar() != '\n'); // Clear input buffer
+
+        if (choice == '#') {
+            return 1;
+        } else if (choice == '0') {
+            printf("Bye! See you again :)\n");
+            return 0;
+        } else if (choice == 'c') {
+            return division();
+        } else {
+            printf("Invalid option! Please try again.\n");
+        }
+    }
+}
+
+int main() {
+    int option;
+
+    do {
+        menu();
+        printf("\nPlease select one option for basic mathematics operations: ");
+        if (scanf("%d", &option) != 1) {
+            printf("Invalid input! Please enter a valid option.\n");
+            while (getchar() != '\n'); // Clear input buffer
+            continue;
+        }
+
+        switch (option) {
+            case 1:
+                if (addition() == 0) return 0;
+                break;
+            case 2:
+                if (subtraction() == 0) return 0;
+                break;
+            case 3:
+                if (multiplication() == 0) return 0;
+                break;
+            case 4:
+                if (division() == 0) return 0;
+                break;
+            case 0:
+                printf("Bye! See you again :)\n");
+                return 0;
+            default:
+                printf("Oops! You may have entered the wrong option. Try again.\n");
+                break;
+        }
+    } while (1);
+
+    return 0;
 }
